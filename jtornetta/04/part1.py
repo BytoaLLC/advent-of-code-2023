dba7e900205078e918f14e8b1713c9588f3ce8da
Josh Tornetta <151060011+jtornetta@users.noreply.github.com>
2023-12-12 10:38:49 -0500

    Corrected regex escape sequence

--- jtornetta/04/part1.py
+++ jtornetta/04/part1.py
@@ -4,7 +4,7 @@ import re
 def sum_card_points(cards):
     total = 0
     for card in range(len(cards)):
-        v = re.findall("[0-9]+|\|", cards[card])
+        v = re.findall("[0-9]+|\\|", cards[card])
         pipe = v.index("|")
         wins = sum(i in v[pipe + 1:] for i in v[1:pipe])
         total += int(2 ** (wins - 1))
